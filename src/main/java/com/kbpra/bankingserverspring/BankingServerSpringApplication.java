7ad907d0fa545f4aaac43fca375f0a54c8766366
Prajwal KB <kbprajwal8@gmail.com>
2023-04-03 22:15:58 +0530

    first commit

---
+++ src/main/java/com/kbpra/bankingserverspring/BankingServerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kbpra.bankingserverspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingServerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kbpra/bankingserverspring/BankingServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbpra.bankingserverspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
